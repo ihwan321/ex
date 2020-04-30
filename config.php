0544be1e05bb554a53ae0310e69ba9996b336ee8
ihwan321 <63368199+ihwan321@users.noreply.github.com>
2020-04-30 08:37:28 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// ISI DENGAN PHPSESSID
+$data1 = "a1239dc7ce3d3efd621dfce719f849a1";
+
+// ISI DENGAN DOGE TOKEN, TRX TOKEN, DAN XRP TOKEN
+$doge = "jPprt3lExlC6Bqd27vUixH63Ol6LppZw";
+$trx = "NWGuQqpdJtTzIomFp8jlrEumMqoGazJa";
+$xrp = "AhLgNNNXqoPS4TV2lXoaolg0GrHpwVJP";
+
+
+
+
+
+
+
+?>
